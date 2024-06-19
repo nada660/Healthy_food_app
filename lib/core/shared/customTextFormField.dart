457829f8477b3991d1bd3495977d673768cdcb1c
nada nemr <nadanmr1123@gmail.com>
2024-06-19 21:54:59 +0300

    solve problem

--- lib/core/shared/customTextFormField.dart
+++ lib/core/shared/customTextFormField.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
 import '../constant/color.dart';
 
--- lib/view/routes.dart
+++ lib/view/routes.dart
@@ -1,15 +1,11 @@
 
-import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
-
-import '../core/class/logoutdialog.dart';
 import '../core/constant/routes.dart';
 import 'screen/authenticated.dart';
 import 'screen/forgetpassword.dart';
 import 'screen/login.dart';
 import 'screen/signup.dart';
 import 'screen/splachscreenview.dart';
-import 'widget/logoutdialog.dart';
 
 List<GetPage<dynamic>>? routes = [
   GetPage(name:AppRoute.SplashScreenView, page: () =>  SplashScreenView()) ,
